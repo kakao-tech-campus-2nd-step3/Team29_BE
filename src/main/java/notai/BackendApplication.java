db7ec04b8ebd74948d6dcc68ae215af1acf2317a
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-09-18 14:13:03 +0900

    Init: 프로젝트 기본 구조 및 공통 컴포넌트 설정
    - 공통 설정 클래스 추가 (JPA, QueryDSL, Swagger, Web)
    - 공통 도메인 엔티티 (RootEntity) 정의
    - 예외 처리 관련 클래스 및 타입 구현
    - JSON 변환을 위한 AttributeConverter 추가
    - 유틸리티 클래스 (Math) 추가

--- src/main/java/notai/BackendApplication.java
+++ src/main/java/notai/BackendApplication.java
@@ -2,12 +2,14 @@ package notai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
