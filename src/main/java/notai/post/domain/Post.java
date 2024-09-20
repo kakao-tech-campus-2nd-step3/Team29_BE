8e9f5e973e7934beadec7151211fe4e34f288c04
Shsin9797 <aaa8141@naver.com>
2024-09-20 12:40:20 +0900

    Feat: Set Post domain entity

--- src/main/java/notai/post/domain/Post.java
+++ src/main/java/notai/post/domain/Post.java
@@ -1,4 +1,32 @@
 package notai.post.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Post {
+    @Id
+    @NotNull
+    private BigInteger id ;
+    @NotNull
+    private BigInteger member_id;
+    @NotNull
+    private String title;
+    @NotNull
+    private String contents;
+    @NotNull
+    private LocalDateTime created_at;
+    @NotNull
+    private LocalDateTime updated_at;
+
 }
