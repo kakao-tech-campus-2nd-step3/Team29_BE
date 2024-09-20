552837680d3f390dc0b75680e571b1fb813b0049
Shsin9797 <aaa8141@naver.com>
2024-09-20 21:21:48 +0900

    Fix: Edit Post entity

--- src/main/java/notai/post/domain/Post.java
+++ src/main/java/notai/post/domain/Post.java
@@ -1,32 +1,44 @@
 package notai.post.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.math.BigInteger;
 import java.time.LocalDateTime;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
 @Entity
 public class Post {
     @Id
-    @NotNull
-    private BigInteger id ;
-    @NotNull
-    private BigInteger member_id;
-    @NotNull
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private Long member_id;
+    @Column(nullable = false)
     private String title;
-    @NotNull
+    @Column(nullable = false)
     private String contents;
-    @NotNull
+    @Column(nullable = false)
     private LocalDateTime created_at;
-    @NotNull
+    @Column(nullable = false)
     private LocalDateTime updated_at;
 
+    public Post(
+            @Auth memberId,
+            String title,
+            String contents
+    ) {
+        this.member_id = memberId;
+        this.title = title;
+        this.contents = contents;
+    }
 }
