214fd3a98edbfa23116a5fbd8e5e76481dde76ea
Shsin9797 <aaa8141@naver.com>
2024-09-20 17:47:44 +0900

    Feat: Set Comment JpaRepository

--- src/main/java/notai/comment/domain/CommentRepository.java
+++ src/main/java/notai/comment/domain/CommentRepository.java
@@ -1,12 +1,11 @@
 package notai.comment.domain;
 
-import notai.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Post, BigInteger> {
+public interface CommentRepository extends JpaRepository<Comment, BigInteger> {
 
 }
