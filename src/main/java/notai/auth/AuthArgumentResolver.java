9eecbac985b0e61faa14f685bdf7535e9d9033fa
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-09-20 17:07:12 +0900

    Feat: JWT 관련 기능 구현
    - Interceptor에서 특정 path를 제외하고 토큰이 유효한지 검증
    - @Auth 어노테이션이 붙어있으면서 Long타입인 파라미터인 경우 ArgumentResolver에서 토큰검증 및 memberId를 반환해주도록 구현

---
+++ src/main/java/notai/auth/AuthArgumentResolver.java
@@ -0,0 +1,50 @@
+package notai.auth;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import notai.common.exception.type.UnAuthorizedException;
+import notai.member.domain.Member;
+import notai.member.domain.MemberRepository;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberRepository memberRepository;
+    private final TokenService tokenService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Auth.class)
+               && parameter.getParameterType().equals(Long.class);
+    }
+
+    @Override
+    public Long resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = extractAccessToken(request);
+        Long memberId = tokenService.extractMemberId(token);
+        return memberRepository.getById(memberId).getId();
+    }
+
+    private static String extractAccessToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        throw new UnAuthorizedException("토큰 정보가 없어 인증에 실패했습니다.");
+    }
+}
