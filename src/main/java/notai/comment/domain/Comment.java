c61c617300d9820b1b8a5d0f08fe17c0d4c56a98
Shsin9797 <aaa8141@naver.com>
2024-09-23 12:42:15 +0900

    Fix : Comment - Member Id 연관관계 매핑

--- src/main/java/notai/comment/domain/Comment.java
+++ src/main/java/notai/comment/domain/Comment.java
@@ -4,11 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import notai.common.domain.RootEntity;
+import notai.member.domain.Member;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -22,8 +25,9 @@ public class Comment extends RootEntity<Long> {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-    @Column(nullable = false)
-    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
     @Column(nullable = false)
     private Long postId;
     @Column(nullable = false)
@@ -31,11 +35,11 @@ public class Comment extends RootEntity<Long> {
     @Column(length = 255, nullable = false)
     private String content;
     public Comment(
-            Long memberId,
+            Member member,
             Long postId,
             String content
     ) {
-        this.memberId = memberId;
+        this.member = member;
         this.postId = postId;
         this.content = content;
     }
